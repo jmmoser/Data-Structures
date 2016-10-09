89102cbf9eddb712aff221d4884f5020270ce995
Justin Moser <jmmoser15@gmail.com>
2016-10-09 15:27:15 -0500

    Use string.h

--- c/byte.h
+++ c/byte.h
@@ -2,11 +2,15 @@
 #define byte_h
 
 #include <stdlib.h>
+#include <string.h>
 
-void byte_copy(char *dst, char *src, int count);
+//void byte_copy(char *dst, char *src, int count);
+//
+//int byte_compare(char *s1, char *s2, int count);
 
 void byte_swap(char *s1, char *s2, int count);
 
-int byte_compare(char *s1, char *s2, int count);
+
+
 
 #endif
