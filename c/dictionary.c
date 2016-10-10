c2b8652fa7d10ae3755c80c272a09108f0563551
Justin Moser <jmmoser15@gmail.com>
2016-10-09 19:54:37 -0500

    Needs testing

--- c/dictionary.c
+++ c/dictionary.c
@@ -1,29 +1,65 @@
 #include "dictionary.h"
 #include "list.h"
+#include <limits.h>
 
 typedef struct dictionary_entry {
     char *key;
-    void *value;
     int element_size;
     void (*deallocator)(void *);
+    void *value;
 } dictionary_entry;
 
-int dictionary_default_hash_function(char *key) {
-    return 1;
+dictionary_entry *dictionary_entry_create(char *key, void *value, int element_size, void (*deallocator)(void *)) {
+    dictionary_entry *entry = malloc(sizeof(dictionary_entry));
+    entry->key = strdup(key);
+    entry->value = malloc(element_size);
+    entry->element_size = element_size;
+    entry->deallocator = deallocator;
+    memcpy(entry->value, value, element_size);
+    return entry;
+}
+
+void dictionary_entry_free(dictionary_entry *entry) {
+    if (entry->deallocator && entry->value) {
+        entry->deallocator(entry->value);
+    }
+    free(entry->key);
+    free(entry->value);
+    free(entry);
+}
+
+int dictionary_default_hash_function(dictionary *d, char *key) {
+    unsigned long int hash;
+    int i = 0;
+    unsigned int len = (unsigned int)strlen(key);
+    
+    while (hash < ULONG_MAX && i < len) {
+        hash = hash << 8;
+        hash += key[i];
+        i++;
+    }
+    
+    return hash % d->array->capacity;
 }
 
 void dictionary_entry_deallocator(void *item) {
-    dictionary_entry *entry = (dictionary_entry *)item;
-    free(entry->key);
-    if (entry->deallocator) {
-        entry->deallocator(entry->value);
-    }
-    free(entry);
+//    dictionary_entry *entry = *(dictionary_entry **)item;
+//    free(entry->key);
+//    if (entry->deallocator) {
+//        entry->deallocator(entry->value);
+//    }
+//    free(entry);
+    dictionary_entry_free(*(dictionary_entry **)item);
+}
+
+void dictionary_array_deallocator(void *item) {
+    list_free(*(list **)item);
 }
 
 dictionary *dictionary_create(int size) {
     dictionary *d = malloc(sizeof(dictionary));
-    d->array = array_create(size, sizeof(dictionary_entry *), dictionary_entry_deallocator);
+    d->array = array_create(size, sizeof(list *), dictionary_array_deallocator);
+    dictionary_change_hash_function(d, dictionary_default_hash_function);
     return d;
 }
 
@@ -33,18 +69,57 @@ void dictionary_free(dictionary *d) {
 }
 
 int dictionary_set(dictionary *d, char *key, void *value, int element_size, void(*deallocator)(void *)) {
-    return 1;
+    if (key) {
+        int hash = d->hash_function(d, key);
+        list *l = *(list **)array_get(d->array, hash);
+        if (!l) {
+            l = list_create();
+            array_set(d->array, hash, &l);
+        }
+        
+        int index = -1;
+        
+        for (list_iterator i = list_get_iterator(l, 0); i.index >= 0; list_iterator_next(&i)) {
+            dictionary_entry *existing_entry = *(dictionary_entry **)i.value;
+            if (strcmp(key, existing_entry->key) == 0) {
+                index = i.index;
+                break;
+            }
+        }
+        
+        if (index >= 0) {
+            list_remove(l, index);
+        }
+        
+        dictionary_entry *entry = dictionary_entry_create(key, value, element_size, deallocator);
+        
+        list_add(l, -1, &entry, sizeof(dictionary_entry *), dictionary_entry_deallocator);
+    }
+    return 0;
 }
 
-int dictionary_get(dictionary *d, char *key) {
-    return 1;
+void *dictionary_get(dictionary *d, char *key) {
+    int hash = d->hash_function(d, key);
+    
+    list *l = *(list **)array_get(d->array, hash);
+    
+    if (l) {
+        for (list_iterator i = list_get_iterator(l, 0); i.index >= 0; list_iterator_next(&i)) {
+            dictionary_entry *entry = *(dictionary_entry **)i.value;
+            if (strcmp(key, entry->key) == 0) {
+                return entry->value;
+            }
+        }
+    }
+    
+    return 0;
 }
 
 int dictionary_remove(dictionary *d, char *key) {
     return 1;
 }
 
-void dictionary_change_hash_function(dictionary *d, int (*hash_function)(char *)) {
+void dictionary_change_hash_function(dictionary *d, int (*hash_function)(dictionary *,char *)) {
     if (hash_function) {
         d->hash_function = hash_function;
     }
--- c/dictionary.h
+++ c/dictionary.h
@@ -7,7 +7,7 @@
  */
 typedef struct dictionary {
     array *array;
-    int (*hash_function)(char *);
+    int (*hash_function)(struct dictionary *, char *);
 } dictionary;
 
 dictionary *dictionary_create(int size);
@@ -16,10 +16,10 @@ void dictionary_free(dictionary *d);
 
 int dictionary_set(dictionary *d, char *key, void *value, int element_size, void(*deallocator)(void *));
 
-int dictionary_get(dictionary *d, char *key);
+void *dictionary_get(dictionary *d, char *key);
 
 int dictionary_remove(dictionary *d, char *key);
 
-void dictionary_change_hash_function(dictionary *d, int (*hash_function)(char *));
+void dictionary_change_hash_function(dictionary *d, int (*hash_function)(dictionary *,char *));
 
 #endif
